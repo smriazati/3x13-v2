66b16185a294e10515988e6dfd7caada62ec8ebd
= <=>
2021-02-09 20:05:16 -0500

    updates for iphone x

--- pages/index.vue
+++ pages/index.vue
@@ -943,7 +943,11 @@ export default {
       },
       meta: [
         { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "viewport",
+          content:
+            "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover",
+        },
         {
           hid: "description",
           name: "description",
